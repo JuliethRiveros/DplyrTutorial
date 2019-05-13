984c886e5c814a600cec3c81c3feee8c2777dffb
Julieth Katherine Riveros Galeano <julieth.riveros.g@gmail.com>
2019-05-12 21:18:49 -0500

    I began writing the slides about the airbnb database.

--- r/analisis_airbnb.r
+++ r/analisis_airbnb.r
@@ -1,73 +1,63 @@
 # Carga la base de datos de arriendos disponibles en Airbnb en Amsterdam en mayo 6 de 2019
 # Fuente: http://insideairbnb.com/get-the-data.html
 # Archivo: 	listings.csv.gz
-# DescripciÃ³n: Detailed Listings data for Amsterdam
+# Descripciín: Detailed Listings data for Amsterdam
 arriendos <- read.csv("data/amsterdam_airbnb.csv")
 
-# funciones a usar: 
-# visualizacion: head + str + plot + 
-# resumenes de datos: summary + 
-# basicas de r: max + var + tapply + subset + by + which + which.min + which.max
-
-# dplyr: solamente funciona con data.frames
-#     summarise + mutate + select + filter + arrange + 
-#     group_by + slice (elige una sola fila) + 
-#     join + (une dos tablas a partir de una columna en comÃºn) +
-#     top_n + tbl_df + distinct + n_distinct
-# cargando la librerÃ­a dplyr
+# Cargar librería dplyr
 library(dplyr)
 
 # Preguntas:
-# 1. Â¿Cuales son los nombres de las columnas?
+# 1. ¿Cuales son los nombres de las columnas?
 names(arriendos)
 # Hay 106 columnas
 
-# 2. Â¿Cuales son los tipos de licencia de arriendamiento?
+# 2. ¿Cuales son los tipos de licencia de arriendamiento?
 arriendos %>%
   distinct(license)
 
-# Â¿Cuales son las polÃ­ticas de cancelaciÃ³n de arriendamiento?
+# ¿Cuales son las políticas de cancelación de arriendamiento?
 arriendos %>%
   distinct(cancellation_policy)
 
-# Â¿En quÃ© ciudades/paises estÃ¡n los arrendatarios?
+# ¿En qué ciudades/paises están los arrendatarios?
 arriendos %>%
   distinct(host_location)
 
-# Â¿En quÃ© barrios estÃ¡n los arriendos?
+# ¿En qué barrios están los arriendos?
 arriendos %>%
   distinct(neighbourhood)
 
-# 3. Â¿Cuales son los 5 arrendatarios con mÃ¡s anuncios de arriendo?
+# 3. ¿Cuales son los 5 arrendatarios con más anuncios de arriendo?
 arriendos %>%
   group_by(host_name) %>%
   summarise(arrendatario = host_name[1], cantidad = length(host_name)) %>%
   arrange(desc(cantidad)) %>%
   top_n(5)
 
-# 4. Â¿Cuales son los 5 arrendatarios que arrienda mÃ¡s noches a un huesped y cuantas noches?
+# 4. ¿Cuales son los 5 arrendatarios que arrienda más noches a un huesped y cuantas noches?
 arriendos %>%
   arrange(desc(maximum_nights)) %>%
   select(host_name, maximum_nights) %>%
   top_n(5)
 
-# 5. Â¿Cuales son los nombres y urls de los 5 arriendos mÃ¡s costosos por noche?
+# 5. ¿Cuales son los nombres y urls de los 5 arriendos más costosos por noche?
 arriendos %>%
   mutate( precio = as.numeric(substr(as.character(price), 2, 10))) %>% 
   arrange(desc(precio)) %>%
   select(precio, price, name, listing_url, host_name) %>%
   top_n(5, precio)
 
-# Â¿Por quÃ© salen mÃ s de 5? Porque hay empates en los precios y segÃºn
-# la documentaciÃ³n de la funciÃ³n top_n: Will include more than n rows if there are ties.
-# Para extraer los 5 primeros podemos usar la funciÃ³n head
+# ¿Por qué salen más de 5? Porque hay empates en los precios y según
+# la documentación de la funciín top_n: Will include more than n rows if there are ties.
+# Para extraer los 5 primeros podemos usar la función head
 head(arriendos %>%
        mutate( precio = as.numeric(substr(as.character(price), 2, 10))) %>% 
        arrange(desc(precio)) %>%
        select(precio, price, name, listing_url, host_name), 5)
 
 
-# 6. Â¿Cuales son los 3 barrios con mayor cantidad de arriendos?
+# 6. ¿Cuales son los 3 barrios con mayor cantidad de arriendos?
 arriendos %>%
   group_by(neighbourhood) %>%
   summarise(barrio = neighbourhood[1], cantidad = length(host_id)) %>%
@@ -75,32 +65,32 @@ arriendos %>%
   top_n(3)
   
 
-# 7. Â¿QuÃ© arriendos tienen menor mÃ¡s baÃ±os?
+# 7. ¿Qué arriendos tienen menor más baños?
 arriendos %>%
   arrange(desc(bathrooms)) %>%
   select(listing_url, bathrooms) %>%
   top_n(5)
 
-# Â¿QuÃ© arriendos tienen menor mÃ¡s camas?
+# ¿Qué arriendos tienen menor más camas?
 arriendos %>%
   arrange(desc(beds)) %>%
   select(listing_url, beds) %>%
   top_n(5)
 
-# 8. Â¿QuÃ© arrendamientos tienen un Ã¡rea mayor a 100 metros cuadrados / 328 pies cuadrados?
+# 8. ¿Qué arrendamientos tienen un área mayor a 100 metros cuadrados / 328 pies cuadrados?
 arriendos %>%
   filter(square_feet>328) %>%
   arrange(desc(square_feet)) %>%
   select(square_feet, listing_url,price)
 
-# 9. Â¿Cuales son los arrendatarios con peor calificaciÃ³n en cuanto a la comunicaciÃ³n?
+# 9. ¿Cuales son los arrendatarios con peor calificación en cuanto a la comunicaciín?
 arriendos %>%
   filter(review_scores_communication < 4) %>%
   select(host_name, review_scores_communication) %>%
   arrange(desc(review_scores_communication)) %>%
   top_n(5)
 
-# 10. Â¿Cuantos arriendos no tienen TV?
+# 10. ¿Cuantos arriendos no tienen TV?
 arriendos %>%
   filter(!grepl("TV", amenities)) %>%
   tally()
