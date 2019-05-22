ea5f1a8a4e011d8d8effc059094bd2bd00ff6da9
JuliethRiveros <julieth.riveros.g@gmail.com>
2019-05-21 20:52:07 -0500

    I added a new R file to explain joins

---
+++ r/joins_explained.r
@@ -0,0 +1,17 @@
+cities <- data.frame("id" = 1:5,
+                     "name" = c("Paris","Tokyo", "Bogotá", "Barcelona", "New York"))
+
+persons <- data.frame("id" = 1:4,
+                      name = c("Cristian", "Jean", "Hikimura", "Steven"),
+                      id_cities = c(3, 1, 2, 5))
+
+# import dplyr
+library(dplyr)
+
+# inner_join
+inner_join(cities, persons, by=c("id" = "id_cities"))
+left_join(cities, persons, by=c("id" = "id_cities"))
+right_join(persons, cities, by=c("id_cities" = "id"))
+
+full_join(persons, cities, by=c("id_cities" = "id"))
+full_join(cities, persons, by=c("id" = "id_cities"))
