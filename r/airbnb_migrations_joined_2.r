c886ab2a00aa5ec7eeb65cf8d3e62561d5e1f0b5
JuliethRiveros <julieth.riveros.g@gmail.com>
2019-05-25 19:44:07 -0500

    ADded 10 quesitons for the joined table

--- r/airbnb_migrations_joined_2.r
+++ r/airbnb_migrations_joined_2.r
@@ -45,3 +45,91 @@ migraciones_filtradas <- migraciones %>%
 # trying to join the databases
 inner_join_arriendos_migraciones_2 <- migraciones_filtradas %>% 
   inner_join(arriendos_filtrados, by=c("CountryHash" = "CountryHash"))
+
+# Question 1
+# How many listings are there from the countrys
+# with more emigrations in 2016
+migraciones_filtradas %>% 
+  filter(Year == 2016) %>%
+  filter(VAR == "B11") %>%
+  group_by(Country) %>%
+  summarise(n=n(), Pais=Country[1], Hash=CountryHash[1]) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n) %>%
+  inner_join(arriendos_filtrados, by=c("Hash"="CountryHash"))
+
+# Question 2: How many countrys are in both tables?
+inner_join_arriendos_migraciones_2 %>%
+  distinct(CountryHash) %>%
+  tally()
+
+# Question 3: Whats the country that appears the most
+# in both tables joined?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(Country.x) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
+# Question 4: Whats the host_name that appears the most
+# in both tables joined?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(host_name) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
+# Question 5: Whats the neighbourhood that appears the most
+# in both tables joined?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(neighbourhood) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
+# Question 6: Whats the price that appears the most
+# in both tables joined?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(price) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
+# Question 7: How many emmigrations had the host_location
+# with more beds in 2015?
+arriendos_filtrados %>%
+  arrange(desc(beds)) %>%
+  top_n(1, beds) %>%
+  select(CountryHash) %>%
+  distinct() %>%
+  inner_join(
+    migraciones_filtradas %>% filter(Year == 2015) %>% filter(VAR == "B11"), by=c("CountryHash"="CountryHash")
+  )
+  
+# Question 8: How many immigrations had the country with more
+# listings in airbnb amsterdam in 2014?
+arriendos_filtrados %>%
+  group_by(CountryHash) %>%
+  summarise(n=n()) %>%
+  top_n(1, n) %>%
+  inner_join(
+    migraciones_filtradas %>%
+      filter(VAR == "B12") %>%
+      filter(Year == 2014),
+      by=c("CountryHash"="CountryHash")
+  )
+
+# Question 9: Whats the Var with more ocurrences in the joined table?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(VAR) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
+# Question 10: Whats the Country of birth with more ocurrences in the joined table?
+inner_join_arriendos_migraciones_2 %>%
+  group_by(Country.of.birth.nationality) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  top_n(1, n)
+
